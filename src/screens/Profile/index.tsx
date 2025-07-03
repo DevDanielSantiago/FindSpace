0b8488a5f4ff1f2a0afa8e572b3ab03bf4b7dee5
Daniel.Souza <daniel.souza@irede.org.br>
2025-07-03 15:45:58 -0400

    feat: profile screen

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -1,13 +1,111 @@
-/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Image, ScrollView, Text, View} from 'react-native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
-import {mainStyles} from './styles';
+import {TouchableRipple} from 'react-native-paper';
+import {headerStyles, mainStyles, optionsStyles, profileStyles} from './styles';
 
 export function ProfileScreen() {
   return (
     <View style={mainStyles.container}>
-      <Text style={{color: 'black'}}>ProfileScreen</Text>
+      <View style={mainStyles.body}>
+        <View style={headerStyles.header}>
+          <View style={headerStyles.headerWrapper}>
+            <Text style={headerStyles.title}>Account</Text>
+          </View>
+        </View>
+
+        <ScrollView>
+          <View style={profileStyles.wrapper}>
+            <Image
+              style={profileStyles.photo}
+              source={require('../../assets/images/photo.png')}
+            />
+            <View style={profileStyles.info}>
+              <Text style={profileStyles.name}>Sophia Carter</Text>
+              <Text style={profileStyles.createdAt}>Joined in 2021</Text>
+            </View>
+          </View>
+
+          <Text style={optionsStyles.title}>Account</Text>
+          <View style={optionsStyles.wrapper}>
+            <View style={optionsStyles.cardIcon}>
+              <MaterialCommunityIcons
+                name="account-outline"
+                size={24}
+                color="#0D141C"
+              />
+            </View>
+            <Text style={optionsStyles.name}>Personal information</Text>
+          </View>
+          <View style={optionsStyles.wrapper}>
+            <View style={optionsStyles.cardIcon}>
+              <MaterialCommunityIcons
+                name="credit-card-outline"
+                size={24}
+                color="#0D141C"
+              />
+            </View>
+            <Text style={optionsStyles.name}>Payment methods</Text>
+          </View>
+          <View style={optionsStyles.wrapper}>
+            <View style={optionsStyles.cardIcon}>
+              <MaterialCommunityIcons
+                name="bell-outline"
+                size={24}
+                color="#0D141C"
+              />
+            </View>
+            <Text style={optionsStyles.name}>Notifications</Text>
+          </View>
+          <TouchableRipple
+            onPress={() => console.log('Pressed')}
+            rippleColor="rgba(0, 0, 0, 0.32)">
+            <View style={optionsStyles.wrapper}>
+              <View style={optionsStyles.cardIcon}>
+                <MaterialCommunityIcons
+                  name="earth"
+                  size={24}
+                  color="#0D141C"
+                />
+              </View>
+              <Text style={optionsStyles.name}>Language</Text>
+            </View>
+          </TouchableRipple>
+
+          <Text style={optionsStyles.title}>Support</Text>
+          <View style={optionsStyles.wrapper}>
+            <View style={optionsStyles.cardIcon}>
+              <MaterialCommunityIcons
+                name="help-circle-outline"
+                size={24}
+                color="#0D141C"
+              />
+            </View>
+            <Text style={optionsStyles.name}>Help center</Text>
+          </View>
+          <View style={optionsStyles.wrapper}>
+            <View style={optionsStyles.cardIcon}>
+              <MaterialCommunityIcons
+                name="file-document-outline"
+                size={24}
+                color="#0D141C"
+              />
+            </View>
+            <Text style={optionsStyles.name}>Terms of service</Text>
+          </View>
+          <View style={optionsStyles.wrapper}>
+            <View style={optionsStyles.cardIcon}>
+              <MaterialCommunityIcons
+                name="shield-outline"
+                size={24}
+                color="#0D141C"
+              />
+            </View>
+            <Text style={optionsStyles.name}>Privacy policy</Text>
+          </View>
+        </ScrollView>
+      </View>
     </View>
   );
 }
--- src/screens/Profile/styles.ts
+++ src/screens/Profile/styles.ts
@@ -9,3 +9,82 @@ export const mainStyles = StyleSheet.create({
     flex: 1,
   },
 });
+
+export const headerStyles = StyleSheet.create({
+  header: {
+    width: '100%',
+    height: 72,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+  },
+  headerWrapper: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    color: '#0D141C',
+  },
+});
+
+export const profileStyles = StyleSheet.create({
+  wrapper: {
+    padding: 16,
+    alignItems: 'center',
+    gap: 16,
+  },
+  photo: {
+    width: 128,
+    height: 128,
+    borderRadius: 64,
+  },
+  info: {
+    alignItems: 'center',
+  },
+  name: {
+    fontSize: 22,
+    lineHeight: 28,
+    fontWeight: 'bold',
+    color: '#0D141C',
+  },
+  createdAt: {
+    fontSize: 16,
+    lineHeight: 24,
+    color: '#4A739C',
+  },
+});
+
+export const optionsStyles = StyleSheet.create({
+  title: {
+    fontSize: 18,
+    lineHeight: 23,
+    fontWeight: 'bold',
+    color: '#0D141C',
+    paddingHorizontal: 16,
+    paddingTop: 16,
+    paddingBottom: 8,
+  },
+  wrapper: {
+    flexDirection: 'row',
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+    alignItems: 'center',
+    gap: 16,
+  },
+  cardIcon: {
+    width: 40,
+    height: 40,
+    borderRadius: 8,
+    backgroundColor: '#E8EDF5',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  name: {
+    fontSize: 16,
+    lineHeight: 24,
+    color: '#0D141C',
+  },
+});
